b46165c5a3e0f1bba047c861078f660258114fd9
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2026-08-18 17:19:59 +0530

    Update create-infra.sh

--- create-infra.sh
+++ create-infra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "IMAGE_VERSION=$IMAGE_VERSION"
-DEXTER=`packer build -var "tenant_id=$(TENANT_ID)" -var "subscription_id=$(SUBSCRIPTION_ID)" -var "client_id=$(CLIENT_ID)" -var "client_secret=$(CLIENT_SECRET)" template.json | tail -7 | head -1`
+DEXTER=`packer build -var image_version=$(IMAGE_VERSION) -var "tenant_id=$(TENANT_ID)" -var "subscription_id=$(SUBSCRIPTION_ID)" -var "client_id=$(CLIENT_ID)" -var "client_secret=$(CLIENT_SECRET)" template.json | tail -7 | head -1`
 image_name=`echo $DEXTER|cut -d ":" -f2`
 echo $image_name
 az vmss update --resource-group vmss-resource-group --name mytestscaleset-1 --set virtualMachineProfile.storageProfile.imageReference.id="${image_name//[[:blank:]]/}"
