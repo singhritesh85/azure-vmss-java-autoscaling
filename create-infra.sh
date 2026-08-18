075aed7a716ff23c631c7c9f5f3f5540a602780b
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2026-08-18 10:56:52 +0530

    Update create-infra.sh

--- create-infra.sh
+++ create-infra.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-az sig create --resource-group ritesh --gallery-name nhimagegallery
-az sig image-definition create --resource-group ritesh --gallery-name nhimagegallery --gallery-image-definition demo --publisher almalinux --offer almalinux-x86_64 --sku 9-gen2 --os-type linux --hyper-v-generation V2
-packer plugins install github.com/hashicorp/azure
-packer plugins install github.com/hashicorp/ansible
 DEXTER=`packer build template.json | tail -7 | head -1`
-image_id=`echo $DEXTER|cut -d ":" -f2`
-echo $image_id
-echo 'variable "image_id" { default = "'${image_id//[[:blank:]]/}'" }' > ../main/newvariable.tf
+image_name=`echo $DEXTER|cut -d ":" -f2`
+echo $image_name
+az vmss update --resource-group vmss-resource-group --name mytestscaleset-1 --set virtualMachineProfile.storageProfile.imageReference.id="${image_name//[[:blank:]]/}"
+az vmss update-instances --instance-ids '*' --resource-group vmss-resource-group --name mytestscaleset-1 --no-wait
